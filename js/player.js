c65cd944fd8891cf58c757b0e5d75e5a9a5ed2ba
Elina <8gelina@gmail.com>
2022-10-09 09:08:25 +0300

    6

--- js/player.js
+++ js/player.js
@@ -62,7 +62,7 @@ function loadTrack(track_index){
     track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
     track_name.textContent = music_list[track_index].name;
     track_artist.textContent = music_list[track_index].artist;
-    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+    now_playing.textContent = "Играет " + (track_index + 1) + " из " + music_list.length;
 
     updateTimer = setInterval(setUpdate, 1000);
 
